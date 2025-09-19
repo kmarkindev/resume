c9f3efd2018b3ae766fc20a0e752f686400d6acf
Kirill Markin <kmarkindev@gmail.com>
2025-09-19 18:30:50 +0300

    Add script to generate PDF version using chrome pdf printer

---
+++ generate_pdf.js
@@ -0,0 +1,27 @@
+import puppeteer from 'puppeteer';
+
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+
+await page.goto('https://kmarkindev.github.io/resume/gamedev', {
+    waitUntil: 'load',
+});
+
+await page.bringToFront();
+
+await page.pdf({
+    path: '_site/testpdf.pdf',
+    displayHeaderFooter: false,
+    printBackground: true,
+    landscape: false,
+    format: 'a4',
+    waitForFonts: true,
+    margin: {
+        top: 25,
+        bottom: 25,
+        left: 25,
+        right: 25
+    }
+});
+
+await browser.close();
